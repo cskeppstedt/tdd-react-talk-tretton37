f4e85d5b92cd11b77fe39a754ad40d0cb7017149
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2017-02-22 10:38:17 +0100

    Set up build config

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const postCSSModulesValues = require('postcss-modules-values')
+const path = require('path')
+const webpack = require('webpack')
+
+const outputPath = path.resolve(__dirname, 'dist')
+const isDevelopment = process.env.NODE_ENV === 'development'
+const localIdentName = isDevelopment ? '[path][local]-[hash:base64:5]' : '[hash:base64:5]'
+const extractCss = new ExtractTextPlugin('[name]-[hash].css')
+
+const plugins = [
+  extractCss,
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+    inject: 'body',
+    chunks: ['bundle']
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify(isDevelopment ? 'development' : 'production')
+    }
+  })
+]
+
+if (!isDevelopment) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false }
+  }))
+}
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js'
+  },
+  devtool: 'source-map',
+  output: {
+    path: outputPath,
+    publicPath: '/',
+    filename: isDevelopment ? '[name]-dev.js' : '[name].js'
+  },
+  devServer: {
+    contentBase: outputPath,
+    port: 9000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: extractCss.extract([
+          'css?modules&importLoaders=1&localIdentName=' + localIdentName,
+          'postcss'
+        ])
+      }
+    ]
+  },
+  plugins: plugins,
+  postcss: [
+    postCSSModulesValues
+  ]
+}
